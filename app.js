a8ccb992ae77088130ab44923feef7608ff0d9b2
krishanjangid <krishanjangid516@gmail.com>
2024-07-09 21:51:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const dotenv = require('dotenv');
+const sequelize = require('./config/db');
+const cors = require('cors');
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(cors({ origin: '*' }));
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(err => console.log('Error: ' + err));
---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+// controllers/userController.js
+const User = require('../models/User');
+
+const createUser = async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const user = await User.create({ name, email });
+    res.status(201).json({ message: 'User created successfully', user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+};
